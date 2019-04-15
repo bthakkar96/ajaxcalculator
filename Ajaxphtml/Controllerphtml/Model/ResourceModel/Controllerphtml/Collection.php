fa3d7880436c74c3ccf1fe6d7ff7ada2897d32fc
bthakkar96 <30663266+bthakkar96@users.noreply.github.com>
2019-04-15 15:23:05 +0530

    Update Collection.php

--- Ajaxphtml/Controllerphtml/Model/ResourceModel/Controllerphtml/Collection.php
+++ Ajaxphtml/Controllerphtml/Model/ResourceModel/Controllerphtml/Collection.php
@@ -7,6 +7,6 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 class Collection extends AbstractCollection{
     protected function _construct()
     {
-        $this->_init('Ajaxphtml\Controllerphtml\Model\Blog', 'Ajaxphtml\Controllerphtml\Model\ResourceModel\Blog');
+        $this->_init('Ajaxphtml\Controllerphtml\Model\Controllerphtml', 'Ajaxphtml\Controllerphtml\Model\ResourceModel\Controllerphtml');
     }
 }
